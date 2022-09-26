c65468964fd464e2728ffc5182f5cdaca599f83b
ESCG\522172 <522172@chrome.escg.ac.uk>
2022-09-26 14:28:15 +0100

    test

---
+++ RPG TEST.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml.Xsl;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string name;
+        Console.WriteLine("What is your name?");
+        name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name + " wlcome to my RPG");
+
+        string encounter;
+        Console.WriteLine("Someone fights you");
+
+    }
+    
+
+   
+}
