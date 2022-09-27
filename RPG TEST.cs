d4544c51453bf2f1877d2c23e3cae7573477c140
ESCG\522172 <522172@chrome.escg.ac.uk>
2022-09-27 16:01:45 +0100

    Fixed code and extended it.

--- RPG TEST.cs
+++ RPG TEST.cs
@@ -5,13 +5,27 @@ public class Program
 {
     public static void Main(string[] args)
     {
+       
         string name;
         Console.WriteLine("What is your name?");
         name = Console.ReadLine();
-        Console.WriteLine("Hello, " + name + " wlcome to my RPG");
+        Console.WriteLine("Hello, " + name + " welcome to my RPG");
 
+        int health = 100;
         string encounter;
-        Console.WriteLine("Someone fights you");
+        Console.WriteLine("Someone fights you, do you run or attack them");
+        encounter = Console.ReadLine();
+        Console.WriteLine("You chose to " + encounter);
+        if (encounter == "attack")
+        {
+            Console.WriteLine("You won the fight!");
+            health = health - 20;
+            Console.WriteLine(health);
+        }
+        else
+        {
+            Console.WriteLine("You ran away");
+        }
 
     }
     
